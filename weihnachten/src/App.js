fe39a405c41fd4a50de4130515e9fb5a812f7b00
NateGantioler <nate.gantioler@gmail.com>
2023-12-19 09:15:13 +0100

    Started Work on the Christmas React App

---
+++ weihnachten/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="bg-weihbg w-screen h-screen">
+      <p class="font-mono text-8xl flex justify-center p-32 font-bold"> Frohe Weihnachten </p>
+    </div>
+  );
+}
+
+export default App;
